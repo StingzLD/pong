fb5f392c630dae5fc13fb987618ee7d59ae7e3d5
Corey Hastings <stingzld@gmail.com>
2023-01-01 14:27:18 -0600

    Created ball class and ball behavior logic

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
+from ball import Ball
 from paddle import Paddle
 from turtle import Screen
+import time
 
 SCREEN_WIDTH = 800
 SCREEN_HEIGHT = 600
@@ -12,20 +14,46 @@ screen.title("PONG")
 screen.tracer(0)
 screen.listen()
 
-# Initialize player 1's paddle
+# Initialize Player 1's paddle
 paddle1 = Paddle(start_x=-360, start_y=0)
 paddle1.speed("fastest")
 screen.onkey(paddle1.move_up, "w")
 screen.onkey(paddle1.move_down, "s")
 
-# Initialize layer 2's paddle
+# Initialize Player 2's paddle
 paddle2 = Paddle(start_x=350, start_y=0)
 paddle2.speed("fastest")
 screen.onkey(paddle2.move_up, "Up")
 screen.onkey(paddle2.move_down, "Down")
 
+# Initialize ball
+ball = Ball()
+
 game_running = True
 while game_running:
+    time.sleep(0.05)
     screen.update()
+    ball.move()
+
+    # Detect collision with wall
+    if ball.ycor() > 280 or ball.ycor() < -280:
+        ball.bounce_y()
+
+    # Detect collision with paddle
+    if ball.xcor() < -330 and ball.distance(paddle1) < 50 or \
+            ball.xcor() > 320 and ball.distance(paddle2) < 50:
+        ball.bounce_x()
+
+    # Detect goal scored
+    if ball.xcor() > 320 and ball.distance(paddle2) > 50:
+        # Player 1 scores
+        pass
+    elif ball.xcor() < -330 and ball.distance(paddle1) > 50:
+        # Player 2 scores
+        pass
+
+    # Reset ball
+    if ball.xcor() > 370 or ball.xcor() < -380:
+        ball.new_ball()
 
 screen.exitonclick()
